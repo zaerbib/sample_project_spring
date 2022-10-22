2ba85fd23e03cb9c57aa5a88e8045e1769857d42
zaerbib <ilunga.joel@yahoo.fr>
2022-10-22 02:32:43 +0200

    About spring security

---
+++ Pss01/src/main/java/com/apress/pss/springsecurity/configuration/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.apress.pss.springsecurity.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
