7f4881b0bc08368ff0525505ef07cbe40e8fdb1c
zaerbib <ilunga.joel@yahoo.fr>
2023-01-22 19:51:14 +0100

    remove other commits

--- pro-spring-boot-2nd-master/ch07/todo-testing/src/test/java/com/apress/todo/ToDoJsonTests.java
+++ pro-spring-boot-2nd-master/ch07/todo-testing/src/test/java/com/apress/todo/ToDoJsonTests.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.context.junit4.SpringRunner;
 
